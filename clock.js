2038a19c975d15d26f9c0989e12c6cfad42e1951
mitushi <mitushi2agrawal@gmail.com>
2021-11-12 18:44:02 +0530

    Added Digital Clock and improved responsiveness

--- clock.js
+++ clock.js
@@ -11,3 +11,14 @@ setInterval(() => {
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
 }, 1000);
+
+let a;
+  let date;
+  let time;
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  setInterval(() => {
+      a = new Date(); 
+      date = a.toLocaleDateString(undefined,options);
+      time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+      document.getElementById('time').innerHTML = time + "<br>" + date;
+  }, 1000);
